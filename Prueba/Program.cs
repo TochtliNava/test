c4c9cf0bf1d1a517dad00adeccfd31e9193a0794
TochtliNava <tochtlinavadev@gmail.com>
2023-11-02 20:01:03 -0600

    Agregar archivos de proyecto.

---
+++ Prueba/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5};
+
+            int sum = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i] % 2;
+            }
+
+            //d = a > b ? (a > c ? a : c) : (b > c? b : c);
+        }
+    }
+}
